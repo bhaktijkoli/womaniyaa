c440fbbcc8dc48d29478ae68a3ed1341e5f75421
Bhaktij Koli <bhaktijkoli121@gmail.com>
2020-05-17 18:03:58 +0530

    Updated home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -96,4 +96,110 @@
         </div>
       </div>
     </section>
+    <section class="section-recommended section-t8">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="title-wrap d-flex justify-content-between">
+              <div class="title-box">
+                <h2 class="title-a">Recommended</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          @php
+          $post = App\Health::join('votes', 'votes.votable_id', 'healths.id')->where('votes.votable_type', 'App\Health')->first();
+          if(!$post) {
+            $post = App\Health::first();
+          }
+          @endphp
+          <div class="col-sm-6">
+            <div class="card-box-a">
+              <div class="img-box-a">
+                <img src="{{Voyager::image($post->image)}}" alt="" class="img-a img-fluid">
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="title-box-d">
+              <h3 class="title-d">{{$post->name}}</h3>
+            </div>
+            <p class="content-d color-text-a">{{Str::limit(strip_tags($post->description), 200)}}</p>
+            <a href="{{route('health.show', ['key' => $post->category->key, 'slug' => $post->slug])}}">Read More <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+          </div>
+        </div>
+        <div class="row">
+          @php
+          $post = App\Food::join('votes', 'votes.votable_id', 'foods.id')->where('votes.votable_type', 'App\Food')->first();
+          if(!$post) {
+            $post = App\Food::first();
+          }
+          $video_id = '';
+          $video = explode("?v=", $post->video);
+          if(count($video) > 1) {
+            $video_id = $video[1];
+          }
+          @endphp
+          <div class="col-sm-6">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/{{$video_id}}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <div class="col-sm-6">
+            <div class="title-box-d">
+              <h3 class="title-d">{{$post->name}}</h3>
+            </div>
+            <p class="content-d color-text-a">{{Str::limit(strip_tags($post->food_groups), 200)}}</p>
+            <a href="{{route('food.show', ['key' => $post->category->key, 'slug' => $post->slug])}}">Read More <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+          </div>
+        </div>
+        <div class="row">
+          @php
+          $post = App\Fitness::join('votes', 'votes.votable_id', 'fitnesses.id')->where('votes.votable_type', 'App\Fitness')->first();
+          if(!$post) {
+            $post = App\Fitness::first();
+          }
+          $video_id = '';
+          $video = explode("?v=", $post->video);
+          if(count($video) > 1) {
+            $video_id = $video[1];
+          }
+          @endphp
+          <div class="row mb-5 foo">
+            <div class="col-sm-6">
+              <iframe width="100%" height="100%" src="https://www.youtube.com/embed/{{$video_id}}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="col-sm-6">
+              <div class="title-box-d">
+                <h3 class="title-d">{{$post->name}}</h3>
+              </div>
+              <p class="content-d color-text-a">{{Str::limit(strip_tags($post->description), 200)}}</p>
+              <a href="{{route('fitness.show', ['key' => $post->category->key, 'slug' => $post->slug])}}">Read More <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          @php
+          $post = App\Yoga::join('votes', 'votes.votable_id', 'yogas.id')->where('votes.votable_type', 'App\Yoga')->first();
+          if(!$post) {
+            $post = App\Yoga::first();
+          }
+          $video_id = '';
+          $video = explode("?v=", $post->video);
+          if(count($video) > 1) {
+            $video_id = $video[1];
+          }
+          @endphp
+          <div class="col-sm-6">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/{{$video_id}}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <div class="col-sm-6">
+            <div class="title-box-d">
+              <h3 class="title-d">{{$post->name}}</h3>
+            </div>
+            <p class="content-d color-text-a">{{Str::limit(strip_tags($post->description), 200)}}</p>
+            <a href="{{route('yoga.show', ['key' => $post->category->key, 'slug' => $post->slug])}}">Read More <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+          </div>
+        </div>
+      </div>
+    </section>
   @endsection
